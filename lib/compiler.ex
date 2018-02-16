3b359656fd631a753b577c085f168aef31674e19
Philip Dexter <philip.dexter@gmail.com>
2018-02-16 15:36:24 +0100

    Work

--- lib/compiler.ex
+++ lib/compiler.ex
@@ -9,9 +9,12 @@ defmodule Aspect.Compiler do
     IO.puts "Compiling #{file}"
 
     file
-    |> File.read!
-    |> :erlang.binary_to_list
-    |> :string.tokens(' \n')
+    |> File.stream!
+    |> Stream.flat_map(&String.split(String.replace_trailing(&1, "\n", ""), " "))
+    |> Stream.zip(Stream.iterate(1, &(&1+1)))
+    |> Stream.filter(fn {s,_} -> s != "" end)
+    |> Stream.map(fn {s,_} -> s end)
+    |> Enum.to_list
     |> compile_forms
   end
 
@@ -50,17 +53,17 @@ defmodule Aspect.Compiler do
       code ++
       [{:eof,7}]
   end
-  def compile_forms(['swap'|ast], [a,b|stack], ctx) do
+  def compile_forms(["swap"|ast], [a,b|stack], ctx) do
     {[], ast, [b,a|stack], ctx}
   end
-  def compile_forms(['+'|ast], [a,b|stack], ctx) do
+  def compile_forms(["+"|ast], [a,b|stack], ctx) do
     # TODO: this assumes + takes 2 arguments
     # and returns 1
     {[x], ctxx} = fresh(1, ctx)
     {[{:match,6,{:var,6,x},{:op,6,:+,{:var,6,a},{:var,6,b}}}],
      ast, [x|stack], ctxx}
   end
-  def compile_forms([':',func_name|ast], stack, ctx) do
+  def compile_forms([":",func_name|ast], stack, ctx) do
     {arg_count, ast_body} = parse_effect(ast)
     {arg_vars, ctxx} = fresh(arg_count, ctx)
     {body_r, ast_rest} = parse_body(ast_body, [])
@@ -79,25 +82,25 @@ defmodule Aspect.Compiler do
 
     # todo ensure arg_count number of variables are left
     # on the stack
-    {[{:function,5,:erlang.list_to_atom(func_name),arg_count,
+    {[{:function,5,String.to_atom(func_name),arg_count,
        [{:clause,5,
          Enum.map(arg_vars, fn var -> {:var, 5, var} end),
          [],
          code}]}],
      ast_rest, stack, ctxxx}
   end
-  def compile_forms(['drop'|ast], [_a|stack], ctx) do
+  def compile_forms(["drop"|ast], [_a|stack], ctx) do
     {[], ast, stack, ctx}
   end
-  def compile_forms(['parse-token',token|ast], stack, ctx) do
+  def compile_forms(["parse-token",token|ast], stack, ctx) do
     {[], ast, [token|stack], ctx}
   end
-  def compile_forms(['['|ast], stack, ctx) do
+  def compile_forms(["["|ast], stack, ctx) do
     {quot_r, ast_rest} = parse_quotation(ast, [])
     quot = Enum.reverse quot_r
     {[], ast_rest, [quot|stack], ctx}
   end
-  def compile_forms(['.'|ast], [x|stack], ctx) do
+  def compile_forms(["."|ast], [x|stack], ctx) do
     {[{:call, 15,
        {:remote, 15, {:atom, 15, :io}, {:atom, 15, :format}},
        [{:string, 15, [126, 112, 126, 110]},
@@ -105,7 +108,7 @@ defmodule Aspect.Compiler do
      ast, stack, ctx}
   end
 
-  def compile_forms(['call('|ast], [quot|stack], ctx) do
+  def compile_forms(["call("|ast], [quot|stack], ctx) do
     {num_args, ast_rest} = parse_effect(ast)
 
     {arg_vars, ctxx} = fresh(num_args, ctx)
@@ -122,11 +125,9 @@ defmodule Aspect.Compiler do
     end
     {code, ctxxx, ret_args} = f.(f, [], quot, arg_vars, ctxx)
 
-    # todo when calling, check if there are args left on stack
-    # and then take them off and match them
-    # (see how callprint doesn't compile)
-    # todo can look at the effect of the function which we're calling
-    # for now let's assume it's either 0 or 1
+    # todo quots that return more than 1
+    # todo can look at the effect of the called quot
+    # for now let's assume it returns either 0 or 1 val
     call = {:call, 12,
             {:fun, 12, {:clauses,
                         [{:clause, 12,
@@ -135,20 +136,18 @@ defmodule Aspect.Compiler do
                           code}]}},
             Enum.map(args_for_call, fn var -> {:var, 12, var} end)}
 
-    {[case ret_args do
-        [] ->
-          call
-        [_] ->
-          {:match, 12, {:var, 12, :A}, call}
-      end],
-     ast_rest, case ret_args do
-                 [] -> stack_rest
-                 [_] -> [:A|stack_rest]
-               end, ctxxx}
+    {code_, stack_} = case ret_args do
+                        [] ->
+                          {call, stack_rest}
+                        [_] ->
+                          {{:match, 12, {:var, 12, :A}, call}, [:A|stack_rest]}
+                      end
+
+    {[code_], ast_rest, stack_, ctxxx}
   end
   def compile_forms([x|ast], stack, ctx) do
     num = try do
-            :erlang.list_to_integer(x)
+            String.to_integer(x)
           rescue
             ArgumentError -> :error
           end
@@ -157,7 +156,7 @@ defmodule Aspect.Compiler do
         # TODO assuming takes 3 args and returns 0
         # fix!!
         [a,b,c|stack_rest] = stack
-        {[{:call,9,{:atom,9,:erlang.list_to_atom(x)},
+        {[{:call,9,{:atom,9,String.to_atom(x)},
            [{:integer,9,a},{:var,9,b},{:var,9,c}]}],
          ast, stack_rest, ctx}
       _ ->
@@ -166,25 +165,25 @@ defmodule Aspect.Compiler do
   end
   def compile_forms([], [], _), do: []
 
-  def parse_body([';'|ast], stack) do
+  def parse_body([";"|ast], stack) do
     {stack, ast}
   end
   def parse_body([token|ast], stack) do
     parse_body(ast, [token|stack])
   end
 
-  def parse_quotation([']'|ast], stack) do
+  def parse_quotation(["]"|ast], stack) do
     {stack, ast}
   end
   def parse_quotation([token|ast], stack) do
     parse_quotation(ast, [token|stack])
   end
 
-  defp parse_effect(['('|ast]) do
+  defp parse_effect(["("|ast]) do
     parse_effect(ast)
   end
-  defp parse_effect([')'|ast]), do: {0, ast}
-  defp parse_effect(['--'|ast]) do
+  defp parse_effect([")"|ast]), do: {0, ast}
+  defp parse_effect(["--"|ast]) do
     {_, next} = parse_effect(ast)
     {0, next}
   end
