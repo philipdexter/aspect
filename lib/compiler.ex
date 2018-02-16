dda5eec210bfd0502c66749327013a5afa45c7d4
Philip Dexter <philip.dexter@gmail.com>
2018-02-16 09:41:08 +0100

    Work

--- lib/compiler.ex
+++ lib/compiler.ex
@@ -34,21 +34,31 @@ defmodule Aspect.Compiler do
   # todo these should all return contexts (and stacks) as well!!!
 
   def compile_forms(ast) do
+    f = fn(f, code, ast, stack, ctx) ->
+      case compile_forms(ast, stack, ctx) do
+        {code_, [], [], _} ->
+          code++code_
+        {code_, ast_, stack_, ctx_} ->
+          f.(f, code++code_, ast_, stack_, ctx_)
+      end
+    end
+    code = f.(f, [], ast, [], %Ctx{})
+
     [{:attribute,1,:file,{"hi.as",1}},
      {:attribute,1,:module,:hi},
      {:attribute,2,:compile,:export_all}] ++
-      compile_forms(ast, [], %Ctx{}) ++
+      code ++
       [{:eof,7}]
   end
   def compile_forms(['swap'|ast], [a,b|stack], ctx) do
-    compile_forms(ast, [b,a|stack], ctx)
+    {[], ast, [b,a|stack], ctx}
   end
   def compile_forms(['+'|ast], [a,b|stack], ctx) do
     # TODO: this assumes + takes 2 arguments
     # and returns 1
     {[x], ctxx} = fresh(1, ctx)
-    [{:match,6,{:var,6,x},{:op,6,:+,{:var,6,a},{:var,6,b}}}]
-    ++ compile_forms(ast, [x|stack], ctxx)
+    {[{:match,6,{:var,6,x},{:op,6,:+,{:var,6,a},{:var,6,b}}}],
+     ast, [x|stack], ctxx}
   end
   def compile_forms([':',func_name|ast], stack, ctx) do
     {arg_count, ast_body} = parse_effect(ast)
@@ -56,34 +66,43 @@ defmodule Aspect.Compiler do
     {body_r, ast_rest} = parse_body(ast_body, [])
     body = Enum.reverse body_r
 
+    f = fn(f, code, ast, stack, ctx) ->
+      case compile_forms(ast, stack, ctx) do
+        {code_, [], [], ctx_} ->
+          {code++code_, ctx_}
+        {code_, ast_, stack_, ctx_} ->
+          f.(f, code++code_, ast_, stack_, ctx_)
+      end
+    end
+    {code, ctxxx} = f.(f, [], body, arg_vars, ctxx)
+
+
     # todo ensure arg_count number of variables are left
     # on the stack
-    [{:function,5,:erlang.list_to_atom(func_name),arg_count,
-      [{:clause,5,
-        Enum.map(arg_vars, fn var -> {:var, 5, var} end),
-        [],
-        compile_forms(body, arg_vars, ctxx)}]}
-     |
-     compile_forms(ast_rest, stack, ctxx)]
+    {[{:function,5,:erlang.list_to_atom(func_name),arg_count,
+       [{:clause,5,
+         Enum.map(arg_vars, fn var -> {:var, 5, var} end),
+         [],
+         code}]}],
+     ast_rest, stack, ctxxx}
   end
   def compile_forms(['drop'|ast], [_a|stack], ctx) do
-    compile_forms(ast, stack, ctx)
+    {[], ast, stack, ctx}
   end
   def compile_forms(['parse-token',token|ast], stack, ctx) do
-    compile_forms(ast, [token|stack], ctx)
+    {[], ast, [token|stack], ctx}
   end
   def compile_forms(['['|ast], stack, ctx) do
     {quot_r, ast_rest} = parse_quotation(ast, [])
     quot = Enum.reverse quot_r
-    compile_forms(ast_rest, [quot|stack], ctx)
+    {[], ast_rest, [quot|stack], ctx}
   end
   def compile_forms(['.'|ast], [x|stack], ctx) do
-    [{:call, 15,
-      {:remote, 15, {:atom, 15, :io}, {:atom, 15, :format}},
-      [{:string, 15, [126, 112, 126, 110]},
-       {:cons, 15, {:var, 15, x}, {:nil, 15}}]}
-     |
-     compile_forms(ast, stack, ctx)]
+    {[{:call, 15,
+       {:remote, 15, {:atom, 15, :io}, {:atom, 15, :format}},
+       [{:string, 15, [126, 112, 126, 110]},
+        {:cons, 15, {:var, 15, x}, {:nil, 15}}]}],
+     ast, stack, ctx}
   end
 
   def compile_forms(['call('|ast], [quot|stack], ctx) do
@@ -93,20 +112,33 @@ defmodule Aspect.Compiler do
 
     {args_for_call, stack_rest} = Enum.split(stack, num_args)
 
+    f = fn(f, code, ast, stack, ctx) ->
+      case compile_forms(ast, stack, ctx) do
+        # TODO assuming call leaves 1 on stack
+        # but now 'okay' doesn't compile!
+        {code_, [], [_], ctx_} ->
+          {code++code_, ctx_}
+        {code_, ast_, stack_, ctx_} ->
+          f.(f, code++code_, ast_, stack_, ctx_)
+      end
+    end
+    {code, ctxxx} = f.(f, [], quot, arg_vars, ctxx)
+
     # todo when calling, check if there are args left on stack
     # and then take them off and match them
     # (see how callprint doesn't compile)
-    [{:match, 12,
-      {:var, 12, :A},
-      {:call, 12,
-       {:fun, 12, {:clauses,
-                   [{:clause, 12,
-                     Enum.map(arg_vars, fn var -> {:var, 12, var} end),
-                     [],
-                     compile_forms(quot, arg_vars, ctxx)}]}},
-       Enum.map(args_for_call, fn var -> {:var, 12, var} end)}}
-     |
-     compile_forms(ast_rest, stack_rest, ctxx)]
+    # todo can look at the effect of the function which we're calling
+    # for now let's assume it's always 1
+    {[{:match, 12,
+       {:var, 12, :A},
+       {:call, 12,
+        {:fun, 12, {:clauses,
+                    [{:clause, 12,
+                      Enum.map(arg_vars, fn var -> {:var, 12, var} end),
+                      [],
+                      code}]}},
+        Enum.map(args_for_call, fn var -> {:var, 12, var} end)}}],
+     ast_rest, [:A|stack_rest], ctxxx}
   end
   def compile_forms([x|ast], stack, ctx) do
     num = try do
@@ -117,11 +149,11 @@ defmodule Aspect.Compiler do
     case num do
       :error ->
         [a,b,c|stack_rest] = stack
-        [{:call,9,{:atom,9,:erlang.list_to_atom(x)},
-          [{:integer,9,a},{:var,9,b},{:var,9,c}]}
-         |
-         compile_forms(ast, stack_rest, ctx)]
-      _ -> compile_forms(ast, [num|stack], ctx)
+        {[{:call,9,{:atom,9,:erlang.list_to_atom(x)},
+           [{:integer,9,a},{:var,9,b},{:var,9,c}]}],
+         ast, stack_rest, ctx}
+      _ ->
+        {[], ast, [num|stack], ctx}
     end
   end
   def compile_forms([], [], _), do: []
